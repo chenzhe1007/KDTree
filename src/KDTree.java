5db9f410d2476c04782c5041116d2e67377c3f1d
chenzhe1007 <zchen1007@gmail.com>
2018-01-26 00:18:32 -0500

    added input/draw function

---
+++ src/KDTree.java
@@ -0,0 +1,178 @@
+/**
+ * Created by Luke on 1/25/18.
+ */
+import com.sun.tools.corba.se.idl.ValueEntry;
+import edu.princeton.cs.algs4.*;
+
+public class KdTree {
+
+    private static class Node {
+        private Point2D p; // the point
+        private RectHV rect; // the axis-aligned rectangle corresponding to this node
+        private Node lb; // the left/bottom subtree
+        private Node rt; // the right/top subtree
+        private int size;
+        private int orientation;
+
+        public Node(Point2D p) {
+            this.p = p;
+            lb = rt = null;
+            this.size = 1;
+        }
+
+        public Node(Point2D p, RectHV rect, int orientation) {
+            this.rect = rect;
+            this.p = p;
+            lb = rt = null;
+            this.size = 1;
+            this.orientation = orientation;
+        }
+    }
+    private static final int HORIZONTAL = 1;
+    private static final int VERTICAL = 0;
+    private Node root;
+
+    public KdTree() {
+        root = null;
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public int size() {
+        if (isEmpty()) {
+            return 0;
+        }
+        return size(root);
+    }
+
+    private int size(Node node) {
+        if (node == null) return 0;
+        return node.size;
+    }
+
+    public void insert(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+        if (contains(p)) {
+            return;
+        }
+        root = insert(root, p, VERTICAL, 0.0, 0.0, 1.0, 1.0);
+    }
+
+    /**
+     *
+     * @param current the current node to compare
+     * @param next new point
+     * @param orientation 0 represents horizontal, 1 represents vertical
+     * @return
+     */
+    private Node insert(Node current, Point2D next, int orientation, double minX, double minY, double maxX, double maxY) {
+        if (current == null) {
+            return new Node(next, new RectHV(minX, minY, maxX, maxY), orientation);
+        }
+        double cmp = 0.0;
+        if (orientation == VERTICAL) {
+            cmp = next.x() - current.p.x();
+            if (cmp >= 0) {
+                current.rt = insert(current.rt, next, HORIZONTAL, current.p.x(), current.rect.ymin(), current.rect.xmax(), current.rect.ymax());
+            } else {
+                current.lb = insert(current.lb, next, HORIZONTAL, current.rect.xmin(), current.rect.ymin(), current.p.x(), current.rect.ymax());
+            }
+        } else {
+            cmp = next.y() - current.p.y();
+            if (cmp >= 0) {
+                current.rt = insert(current.rt, next, VERTICAL, current.rect.xmin(), current.p.y(), current.rect.xmax(), current.rect.ymax());
+            } else {
+                current.lb = insert(current.lb, next, VERTICAL, current.rect.xmin(), current.rect.ymin(), current.rect.xmax(), current.p.y());
+            }
+        }
+
+        current.size = size(current.lb) + size(current.rt) + 1;
+        return current;
+    }
+
+
+
+    public boolean contains(Point2D p) {
+        Node cur = root;
+        int i = 0;
+        while (cur != null) {
+            if (cur.p.x() == p.x() && cur.p.y() == p.y()) {
+                return true;
+            }
+            if (i % 2 == 0) {
+                if (p.x() >= cur.p.x()) {
+                    cur = cur.rt;
+                } else {
+                    cur = cur.lb;
+                }
+            } else {
+                if (p.y() >= cur.p.y()) {
+                    cur = cur.rt;
+                } else {
+                    cur = cur.lb;
+                }
+            }
+            i++;
+        }
+        return false;
+    }
+
+    public void draw() {
+        Queue<Node> queue = new Queue<Node>();
+        preOrder(queue, root);
+        for (Node node : queue) {
+            StdDraw.setPenRadius();
+            if (node.orientation == VERTICAL) {
+                StdDraw.setPenColor(StdDraw.RED);
+                Point2D tr = new Point2D(node.p.x(), node.rect.ymax());
+                tr.drawTo(new Point2D(node.p.x(), node.rect.ymin()));
+            } else if (node.orientation == HORIZONTAL){
+                StdDraw.setPenColor(StdDraw.BLUE);
+                Point2D tr = new Point2D(node.rect.xmin(), node.p.y());
+                tr.drawTo(new Point2D(node.rect.xmax(), node.p.y()));
+            }
+            StdDraw.setPenColor(StdDraw.BLACK);
+            StdDraw.setPenRadius(0.01);
+            node.p.draw();
+
+        }
+    }
+
+    private void preOrder(Queue<Node> queue, Node root) {
+        if (root == null) return;
+
+        queue.enqueue(root);
+        preOrder(queue, root.lb);
+        preOrder(queue, root.rt);
+    }
+
+    public Iterable<Point2D> range(RectHV rectHV) {
+        return new Queue<Point2D>();
+    }
+
+    public Point2D nearest(Point2D p) {
+        return new Point2D(0.1,0.1);
+    }
+
+    public static void main(String[] args) {
+        String filename = args[0];
+        In in = new In(filename);
+        //PointSET brute = new PointSET();
+        KdTree kdtree = new KdTree();
+        while (!in.isEmpty()) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+
+            kdtree.insert(p);
+            //StdOut.println(kdtree.contains(p));
+
+            //kdtree.contains(p);
+            //StdOut.println(kdtree.size());
+        }
+        kdtree.draw();
+        StdDraw.show();
+    }
+}
---
+++ src/PointSet.java
@@ -0,0 +1,75 @@
+/**
+ * Created by Luke on 1/25/18.
+ */
+import edu.princeton.cs.algs4.*;
+
+import java.util.TreeSet;
+
+public class PointSET {
+
+    TreeSet<Point2D> set;
+    public PointSET() {
+        set = new TreeSet<Point2D>();
+    }
+
+    public boolean isEmpty() {
+        return set.size() == 0;
+    }
+
+    public int size() {
+        return set.size();
+    }
+
+    public void insert(Point2D p) {
+        set.add(p);
+    }
+
+    public boolean continas(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+        return set.contains(p);
+    }
+
+    public void draw() {
+        for (Point2D point : set) {
+            point.draw();
+        }
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new IllegalArgumentException();
+        Queue<Point2D> queue = new Queue<Point2D>();
+        for (Point2D point : set) {
+            if (point.x() <= rect.xmax() && point.x() >= rect.xmin() && point.y() <= rect.ymax() && point.y() >= rect.ymin()) {
+                queue.enqueue(point);
+            }
+        }
+        return queue;
+    }
+
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new IllegalArgumentException();
+        double minDis = Double.MAX_VALUE;
+        Point2D ans = null;
+        for (Point2D point : set) {
+            double dis = point.distanceTo(p);
+            if (dis < minDis) {
+                minDis = dis;
+                ans = point;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String filename = args[0];
+        In in = new In(filename);
+        PointSET brute = new PointSET();
+        //KdTree kdtree = new KdTree();
+        while (!in.isEmpty()) {
+            double x = in.readDouble();
+            double y = in.readDouble();
+            Point2D p = new Point2D(x, y);
+            brute.insert(p);
+        }
+    }
+}
